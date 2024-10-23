ad4722e8fe9f039ed9764b4979b9173e14a58ba5
unknown <joe2sure1@gmail.com>
2024-10-23 23:02:09 +0100

    refactor the backend server.js middleware and also the frontend Api login functionality in the frontend

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,6 +1,6 @@
 import { apiRequest } from './apiUtils';
 
 export const authApi = {
-  login: (email, password) => apiRequest('/auth/login', 'POST', { email, password }),
-  signup: (username, email, password, confirmPassword) => apiRequest('/auth/register', 'POST', { username, email, password, confirmPassword }),
+  login: (email, password) => apiRequest('auth/login', 'POST', { email, password }),
+  signup: (username, email, password, confirmPassword) => apiRequest('auth/register', 'POST', { username, email, password, confirmPassword }),
 };
