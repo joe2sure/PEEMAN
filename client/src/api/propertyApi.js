341eae65dabc098be0ce7bb66cecbcf83ecb0f0b
unknown <joe2sure1@gmail.com>
2024-10-29 09:26:54 +0100

    resolve the create-property error by refatoring the apiUtils.js code snippet

--- client/src/api/propertyApi.js
+++ client/src/api/propertyApi.js
@@ -6,16 +6,3 @@ export const propertyApi = {
   updateProperty: (id, propertyData) => apiRequest(`/admin/properties/${id}`, 'PUT', propertyData),
   deleteProperty: (id) => apiRequest(`/admin/properties/${id}`, 'DELETE'),
 };
-
-
-// import { apiRequest } from './apiUtils';
-
-// export const propertyApi = {
-//   fetchProperties: () => apiRequest('/properties').catch(error => {
-//     console.error('Error in fetchProperties:', error);
-//     throw error;
-//   }),
-//   addProperty: (property) => apiRequest('/admin/properties', 'POST', property),
-//   updateProperty: (id, property) => apiRequest(`/admin/properties/${id}`, 'PUT', property),
-//   deleteProperty: (id) => apiRequest(`/admin/properties/${id}`, 'DELETE'),
-// };
